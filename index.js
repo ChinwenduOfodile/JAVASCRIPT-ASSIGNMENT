f6c3376648ddd68de09f071e8ca8abb0f36f990b
Chinwendu Ofodile <chinwenduofodile@hashnode.com>
2023-05-12 07:51:28 +0100

    MY FIRST JAVASCRIPT COMIT

---
+++ index.js
@@ -0,0 +1,6 @@
+let updateMessageEl = document.querySelector("#update-message")
+let btnEl = document.querySelector("#btn")
+
+function update() { 
+    updateMessageEl.textContent = "I AM CURRENTLY LEARNING JAVASCRIPT"
+}
